33a92c457588eb0fd8a2cdf6a0f6c63dd7f11e56
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-02 19:52:00 +0300

    start backend🥰

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: [
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:3030',
+      'http://localhost:3030',
+    ],
     credentials: true,
   }
   app.use(cors(corsOptions))
